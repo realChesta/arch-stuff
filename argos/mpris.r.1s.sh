b5fe7739bffabe0f1eb63775c9087c2f3b6feaba
realChesta <realChesta@users.noreply.github.com>
2020-02-10 12:19:07 +0100

    Added custom argos extension for showing current music

---
+++ argos/mpris.r.1s.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+status="$(playerctl status 2>&1)"
+current=$(playerctl metadata --format "{{ title }} ~ {{ artist }}")
+
+if (echo "$status" | grep -q "Stopped") || (echo "$status" | grep -q "No players"); then
+    echo "whew"
+    exit
+fi
+
+if echo "$status" | grep -q "Paused"; then
+    status_char="⏸"
+    play_pause="Play | iconName=media-playback-start-symbolic bash='playerctl play' terminal=false"
+elif echo "$status" | grep -q "Playing"; then
+    status_char="⏵"
+    play_pause="Pause | iconName=media-playback-pause-symbolic bash='playerctl pause' terminal=false"
+fi
+
+echo "$status_char $current\n---"
+echo "Next | iconName=media-skip-forward-symbolic bash='playerctl next' terminal=false"
+echo "$play_pause"
+echo "Previous | iconName=media-skip-backward-symbolic bash='playerctl previous' terminal=false"
